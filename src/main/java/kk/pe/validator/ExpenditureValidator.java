1236f430a7e7393c006e2fffb31804231aea313b
khoale1234 <93108830+khoale1234@users.noreply.github.com>
2024-06-06 20:06:28 +0700

    ket noi them khoan chi voi csdl

---
+++ src/main/java/kk/pe/validator/ExpenditureValidator.java
@@ -0,0 +1,35 @@
+package kk.pe.validator;
+
+import javax.swing.JComboBox;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import kk.pe.entity.ExpenditureType;
+
+public class ExpenditureValidator {
+        public static String validate(JTextField txtName, JTextField txtAmount,
+                JTextField txtDate, JComboBox<ExpenditureType> cbxType){
+        StringBuilder sb = new StringBuilder();
+            
+        if(Validator.isEmpty(txtName)){
+            sb.append("Name must be entered\n");
+        }
+        
+        if(Validator.isEmpty(txtAmount)){
+            sb.append("Amount must be entered\n");
+        }
+        
+        if(Validator.isMin(txtAmount,0)){
+            sb.append("Amount must be greater than 0 or invalid number\n");
+        }
+        
+        if(Validator.isEmpty(txtDate)){
+            sb.append("Date must be entered\n");
+        }
+        
+        if(!Validator.isDate(txtDate)){
+            sb.append("Invalid date\n");
+        }
+        
+        return sb.isEmpty()? null : sb.toString();
+    }
+}
--- src/main/java/kk/pe/validator/Validator.java
+++ src/main/java/kk/pe/validator/Validator.java
@@ -1,13 +1,68 @@
 package kk.pe.validator;
 
 import java.awt.Color;
+import java.util.Date;
 import javax.swing.JComponent;
 import javax.swing.JTextField;
+import kk.pe.util.DateUtil;
 
 public class Validator {
     public static boolean isEmpty(JComponent component){
         if(component instanceof JTextField){
             JTextField txt = (JTextField) component;
+            if(txt.getText().equals("")){
+                txt.setBackground(Color.yellow);
+                return true;
+            }else{
+                txt.setBackground(Color.white);
+            }
+        }
+        return false;
+    }
+    
+    public static boolean isMin(JComponent component, double min){
+        if(component instanceof JTextField){
+            JTextField txt = (JTextField) component;
+            
+            try{
+                double value = Double.parseDouble(txt.getText());
+                
+                if(value >= min){
+                    txt.setBackground(Color.white);
+                    return true;
+                }
+                txt.setBackground(Color.yellow);
+                
+            } catch (Exception e){
+                txt.setBackground(Color.yellow);
+            }
+            
+            if(txt.getText().equals("")){
+                txt.setBackground(Color.yellow);
+                return true;
+            }else{
+                txt.setBackground(Color.white);
+            }
+        }
+        return false;
+    }
+    
+    public static boolean isDate(JComponent component){
+        if(component instanceof JTextField){
+            JTextField txt = (JTextField) component;
+            
+            try{
+                DateUtil dateUtil = new DateUtil();
+                Date date = dateUtil.toDate(txt.getText());
+                
+                
+                txt.setBackground(Color.white);
+                return true;
+
+            } catch (Exception e){
+                txt.setBackground(Color.yellow);
+            }
+            
             if(txt.getText().equals("")){
                 txt.setBackground(Color.yellow);
                 return true;
